7427340dd2c3f94710f5c8e02fd2ebe80749b9ce
Stratos Foteinopoulos <s.foteinopoulos@kaizengaming.com>
2022-08-25 18:20:03 +0300

    d

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editss <code>src/App.js</code> and save to reload.
+          Editsssss <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
